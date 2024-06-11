64da7fa8ce229c0cac0509514b5d83af4246f6b1
Berion999 <berionsalgado@gmail.com>
2024-06-11 15:33:53 -0300

    Loja Virtual

---
+++ loja_virtual_1/src/main/java/com/example/demo/LojaVirtual1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtual1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtual1Application.class, args);
+	}
+
+}
